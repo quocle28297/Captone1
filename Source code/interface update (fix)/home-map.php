1d34b479db96d1f0d1b2ab22c9486c115a9c0258
Quocle282 <nhoxpro28297@gmail.com>
2018-11-20 21:38:58 +0700

    update system
    update

--- Source code/interface update (fix)/home-map.php
+++ Source code/interface update (fix)/home-map.php
@@ -119,9 +119,9 @@ $loginURL = $helper->getLoginUrl($redirectURL, $permissions);
 
     <!-- Stylesheets
         ============================================= -->
-        <link href="css/external.css" rel="stylesheet">
+        <link href="css/external_nhat.css" rel="stylesheet">
         <link href="css/bootstrap.min.css" rel="stylesheet">
-        <link href="css/style.css" rel="stylesheet">
+        <link href="css/style_nhat.css" rel="stylesheet">
         <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
         <!--icon facebook
         -->
